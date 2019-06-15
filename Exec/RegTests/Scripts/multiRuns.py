c0ffa409f3fa5acbef9409370af470166b67b474
Lucas Esclapez <lesclapez@lbl.gov>
2019-06-15 00:36:27 -0700

    [Testing] Update the multirun.py and pprocConvOrder.py.

--- Exec/RegTests/Scripts/multiRuns.py
+++ Exec/RegTests/Scripts/multiRuns.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
-# Script to lunch several times PeleLM at different resolution
+# A template script to lunch several times PeleLM at different resolution
 # in order to evaluate the convergence order
+# Used in the marex regression_testing framework
 
-# ./multiRuns.py testName PeleLMInputFile
+# Usage:
+#   ./multiRuns.py testName PeleLMInputFile
+
+# Input:
+#   * testName: a TESTNAME that will be prependded to the plt files names
+#   * PeleLMInputFile: the PeleLM input file
+
+# "Internal" user input
+#   * resolution : a list of the resolutions to run
+
+# Head's up : 
+#   * The PeleLM executable is searched for in the current directory.
 
 import sys
 import os
@@ -13,6 +25,8 @@ import numpy as np
 def multiRun(test_name,inputFile):
     
     print(" Scripted multiple runs for convergence study ")
+    # User data
+    resolution = [32,64,128,256]        
 
     # Get the PeleLM exec
     run_dir = os.getcwd()
@@ -20,21 +34,12 @@ def multiRun(test_name,inputFile):
         if ( f.startswith("PeleLM") and f.endswith(".ex")):
                executable = f
 
-    # Loop on /= resolutions, run & prepare fcompare input
-    resolution = [32,64,128]        
+    # Loop on /= resolutions, run 
     for case in resolution:
         outfile = "{}_{}.run.out".format(test_name,case)
         runtime_params = "amr.n_cell={} {} {} ".format(case,case,case)
         runtime_params += "amr.plot_file={}_plt_{}_".format(test_name,case)
-#        os.system("mpiexec -n 1 ./{} {} {} > {}".format(executable, inputFile,runtime_params, outfile))
-        pltfile=[]
-        for f in os.listdir(run_dir):
-            if (f.startswith("{}_plt_{}_".format(test_name,case))):
-                pltfile.append(f)
-        pltfile.sort()        
-        f = open("fcompare_params_{}".format(case),"w")
-        f.write(" -n 2 {} {}".format(pltfile[0], pltfile[-1]))
-        f.close()
+        os.system("mpiexec -n 1 ./{} {} {} > {}".format(executable, inputFile,runtime_params, outfile))
 
 
 if __name__ == "__main__":
