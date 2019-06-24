e35362b09e718fee58e9e255ebd4fe9f8c31fafe
Lucas Esclapez <lesclapez@lbl.gov>
2019-06-23 23:22:46 -0700

    [Testing] Update scripts for convergence testing in FlameSheet_2steps.

--- Exec/RegTests/FlameSheet_2steps/multiRuns.py
+++ Exec/RegTests/FlameSheet_2steps/multiRuns.py
@@ -5,10 +5,10 @@
 # Used in the marex regression_testing framework
 
 # Usage:
-#   ./multiRuns.py testName PeleLMInputFile
+#   ./multiRuns.py --test_name DummyTest --input_file PeleLMInputFile
 
 # Input:
-#   * testName: a TESTNAME that will be prependded to the plt files names
+#   * test_name: a TESTNAME that will be prependded to the plt files names
 #   * PeleLMInputFile: the PeleLM input file
 
 # "Internal" user input
@@ -20,9 +20,14 @@
 import sys
 import os
 import shutil
+import argparse
 import numpy as np
 
-def multiRun(test_name,inputFile):
+USAGE = """    
+    A template script to launch several times PeleLM.
+"""
+
+def multiRun(args):
     
     print(" Scripted multiple runs for convergence study ")
     # User data
@@ -33,18 +38,43 @@ def multiRun(test_name,inputFile):
     for f in os.listdir(run_dir):
         if ( f.startswith("PeleLM") and f.endswith(".ex")):
                executable = f
+    
+    # Check the test name: current folder name is default
+    if ( args.test_name == "None" ):
+        args.test_name = run_dir.split("/")[-1]
+
+    # Check for the input file: first inputs.* is default
+    if ( args.input_file == "None" ):
+        for f in os.listdir(run_dir):
+            if ( f.startswith("inputs") ):
+                args.input_file = f
+                break
 
     # Loop on /= resolutions, run 
     for case in resolution:
         width = case//16
         print(" Running {}x{} case".format(width,case))
-        outfile = "{}_{}.run.out".format(test_name,case)
+        outfile = "{}_{}.run.out".format(args.test_name,case)
         runtime_params = "amr.n_cell={} {} {} ".format(width,case,case)
-        runtime_params += "amr.plot_file={}_plt_{}_".format(test_name,case)
-        os.system("mpiexec -n 1 ./{} {} {} > {}".format(executable, inputFile,runtime_params, outfile))
+        runtime_params += "amr.plot_file={}_plt_{}_".format(args.test_name,case)
+        os.system("mpiexec -n 1 ./{} {} {} > {}".format(executable, args.input_file, runtime_params, outfile))
 
+def parse_args(arg_string=None):
+    parser = argparse.ArgumentParser(description=USAGE)
+
+    parser.add_argument("--test_name", type=str, default="None", metavar="test-name",
+                        help="name of the test. Default = current folder name")
+
+    parser.add_argument("--input_file", type=str, default="None", metavar="Pele-input",
+                        help="input file name. Default = first inputs.* in current directory")
+
+    if not arg_string is None:
+        args = parser.parse_args(arg_string)
+    else:
+        args = parser.parse_args()
+
+    return args   
 
 if __name__ == "__main__":
-    test_name = str(sys.argv[1])
-    inputFile = str(sys.argv[2])
-    multiRun(test_name,inputFile)
+    args = parse_args(arg_string=sys.argv[1:])
+    multiRun(args)
