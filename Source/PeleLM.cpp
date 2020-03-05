eed9d042db96d623cbf739c74e1f2305f164dbf2
Lucas Esclapez <lesclapez@lbl.gov>
2020-03-04 18:07:07 -0800

    Use EB_set_covered_face in calcViscosity too.

--- Source/PeleLM.cpp
+++ Source/PeleLM.cpp
@@ -8219,11 +8219,21 @@ PeleLM::calcViscosity (const Real time,
   MultiFab **mf_ec = fb.get();
 
 #ifdef AMREX_USE_EB
+//Get typical values for T and rhoY
+  Vector<Real> typvals;
+  typvals.resize(nComp);
+  typvals[Tcomp] = typical_values[Temp];
+  for (int k = 0; k < nspecies; ++k) {
+     typvals[RYcomp+k] = typical_values[first_spec+k]*typical_values[Density];
+  }
+
   auto math_bc_T = fetchBCArray(State_Type,Temp,1);
   EB_interp_CC_to_FaceCentroid(mf_cc, D_DECL(*mf_ec[0],*mf_ec[1],*mf_ec[2]), Tcomp, Tcomp, 1, geom, math_bc_T);
+  EB_set_covered_faces({D_DECL(mf_ec[0],mf_ec[1],mf_ec[2])},Tcomp,1,typvals);
 
   auto math_bc_RY = fetchBCArray(State_Type,first_spec,nspecies);
   EB_interp_CC_to_FaceCentroid(mf_cc, D_DECL(*mf_ec[0],*mf_ec[1],*mf_ec[2]), RYcomp, RYcomp, nspecies, geom, math_bc_RY);
+  EB_set_covered_faces({D_DECL(mf_ec[0],mf_ec[1],mf_ec[2])},RYcomp,nspecies,typvals);
 
 #ifdef _OPENMP
 #pragma omp parallel
