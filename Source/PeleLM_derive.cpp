cf58a5cb8e401218ee76d1010bba5b9cb6e0d7ee
Emmanuel Motheau <emotheau@lbl.gov>
2020-06-13 18:08:09 -0700

    forgot to add the new routines

---
+++ Source/PeleLM_derive.cpp
@@ -0,0 +1,74 @@
+#include "PeleLM.H"
+#include "PeleLM_derive.H"
+
+using namespace amrex;
+
+
+//
+// Multiply by Rho
+//
+
+void pelelm_dermprho (const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                  const FArrayBox& datfab, const Geometry& /*geomdata*/,
+                  Real /*time*/, const int* /*bcrec*/, int /*level*/)
+
+{
+    auto const in_dat = datfab.array();
+    auto       der = derfab.array();
+    amrex::ParallelFor(bx,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {
+        der(i,j,k,dcomp) = in_dat(i,j,k,1)*in_dat(i,j,k,0);
+    });
+}
+
+//
+// Divide by Rho
+//
+
+void pelelm_derdvrho (const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                  const FArrayBox& datfab, const Geometry& /*geomdata*/,
+                  Real /*time*/, const int* /*bcrec*/, int /*level*/)
+
+{
+    auto const in_dat = datfab.array();
+    auto       der = derfab.array();
+    amrex::ParallelFor(bx,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {
+        der(i,j,k,dcomp) = in_dat(i,j,k,1)/in_dat(i,j,k,0);
+    });
+}
+
+
+//
+// Extract species mass rhoY_n
+//
+
+void pelelm_derRhoY (const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
+                  const FArrayBox& datfab, const Geometry& /*geomdata*/,
+                  Real /*time*/, const int* /*bcrec*/, int /*level*/)
+
+{
+    auto const in_dat = datfab.array();
+    auto       der = derfab.array();
+    amrex::ParallelFor(bx, ncomp,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+    {
+        der(i,j,k,n) = in_dat(i,j,k,n);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
