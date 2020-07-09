89e8adbae89fe0fb67c7b0f41687997b36e5813e
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-07-08 19:59:45 -0700

    fix some gpu issues

--- Source/PeleLM_bcfill.cpp
+++ Source/PeleLM_bcfill.cpp
@@ -17,7 +17,7 @@ struct PeleLMPressFillExtDirGPU
                      const int orig_comp) const
         {
             // do something for external Dirichlet (BCType::ext_dir)
-amrex::Print() << "\n \n HELLO from PeleLMPressFillExtDirGPU \n \n ";
+            printf("\n \n HELLO from PeleLMPressFillExtDirGPU \n \n ");
         }
 };
 
