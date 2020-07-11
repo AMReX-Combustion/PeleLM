9c79f7c1d9b96f53ada981a71d5cdea70c0a125c
Emmanuel Motheau <emotheau@lbl.gov>
2020-07-10 21:04:29 -0700

    removing obsolete stuff

--- Source/PeleLM_setup.cpp
+++ Source/PeleLM_setup.cpp
@@ -616,33 +616,25 @@ PeleLM::variableSetUp ()
   Vector<BCRec>       bcs(BL_SPACEDIM);
   Vector<std::string> name(BL_SPACEDIM);
 
-//  int do_group_bndry_fills = 0;
-//  ppns.query("do_group_bndry_fills",do_group_bndry_fills);
-
   set_x_vel_bc(bc,phys_bc);
   bcs[0]  = bc;
   name[0] = "x_velocity";
-//  if (do_group_bndry_fills==0) desc_lst.setComponent(State_Type,Xvel,"x_velocity",bc,pelelm_bndryfunc);
 
   set_y_vel_bc(bc,phys_bc);
   bcs[1]  = bc;
   name[1] = "y_velocity";
-//  if (do_group_bndry_fills==0) desc_lst.setComponent(State_Type,Yvel,"y_velocity",bc,pelelm_bndryfunc);
 
 #if(AMREX_SPACEDIM==3)
   set_z_vel_bc(bc,phys_bc);
   bcs[2]  = bc;
   name[2] = "z_velocity";
-//  if (do_group_bndry_fills==0) desc_lst.setComponent(State_Type,Zvel,"z_velocity",bc,pelelm_bndryfunc);
 #endif
 
-//  if (do_group_bndry_fills!=0) {
-    desc_lst.setComponent(State_Type,
-                          Xvel,
-                          name,
-                          bcs,
-                          pelelm_bndryfunc);
-//  }
+  desc_lst.setComponent(State_Type,
+                        Xvel,
+                        name,
+                        bcs,
+                        pelelm_bndryfunc);
 
 
 
