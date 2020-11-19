f3b6540bb991a9712c0fb0b6dbefa96ed9194ec2
Lucas Esclapez <lesclapez@lbl.gov>
2020-11-18 23:33:27 -0800

    Try with Cuda 10 to avoid missing CuSparse needed for Sundials.

--- .github/workflows/dependencies/dependencies_nvcc.sh
+++ .github/workflows/dependencies/dependencies_nvcc.sh
@@ -26,5 +26,5 @@ sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x8
 sudo apt-key add 7fa2af80.pub
 echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
-sudo apt-get install -y cuda-command-line-tools-11-0 cuda-compiler-11-0 cuda-cupti-dev-11-0 cuda-minimal-build-11-0 cuda-nvml-dev-11-0 cuda-nvtx-11-0 libcurand-dev-11-0
-sudo ln -s cuda-11.0 /usr/local/cuda
+sudo apt-get install -y cuda-command-line-tools-10-0 cuda-compiler-10-0 cuda-cupti-dev-10-0 cuda-minimal-build-10-0 cuda-nvml-dev-10-0 cuda-nvtx-10-0 libcurand-dev-10-0
+sudo ln -s cuda-10.0 /usr/local/cuda
