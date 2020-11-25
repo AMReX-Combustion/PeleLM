cb069a8f2a1a216c975e5e8d432d35c13ec95654
Lucas Esclapez <lesclapez@lbl.gov>
2020-11-25 14:17:39 -0800

    Update EB_cases/EB_FlowPastCylinder and EB_cases/EB_FlamePastCylinder to comply with changes associated with PmfData, clean up input files.

--- Exec/EB_cases/EB_FlowPastCylinder/pelelm_prob.cpp
+++ Exec/EB_cases/EB_FlowPastCylinder/pelelm_prob.cpp
@@ -1,13 +1,17 @@
 
-#include <pelelm_prob.H>
+#include <AMReX_PROB_AMR_F.H>
+#include "pelelm_prob_parm.H"
+#include <AMReX_ParmParse.H>
 
 namespace ProbParm
 {
-   AMREX_GPU_DEVICE_MANAGED  amrex::Real P_mean = 101325.0;
-   AMREX_GPU_DEVICE_MANAGED  amrex::Real T_mean = 298.0;
-   AMREX_GPU_DEVICE_MANAGED  amrex::Real MeanFlow = 3.0;
-   AMREX_GPU_DEVICE_MANAGED  int         FlowDir = AMREX_SPACEDIM - 1;
-} // namespace ProbParm
+    AMREX_GPU_DEVICE_MANAGED  amrex::Real meanFlowMag = 3.0;
+    AMREX_GPU_DEVICE_MANAGED  amrex::Real T_mean = 298.0;
+    AMREX_GPU_DEVICE_MANAGED  amrex::Real P_mean = 101325.0;
+    AMREX_GPU_DEVICE_MANAGED  amrex::Real xblob  = 0.0;
+    AMREX_GPU_DEVICE_MANAGED  amrex::Real delta_blob  = 2e-1;
+    AMREX_GPU_DEVICE_MANAGED  amrex::Real RC  = 0.02e-1;
+}
 
 extern "C" {
     void amrex_probinit (const int* init,
@@ -17,11 +21,13 @@ extern "C" {
                          const amrex_real* probhi)
     {
         amrex::ParmParse pp("prob");
-        pp.query("P_mean", ProbParm::P_mean);
-        pp.query("T_mean", ProbParm::T_mean);
-        pp.query("MeanFlow", ProbParm::MeanFlow);
-        pp.query("FlowDir",  ProbParm::FlowDir);
 
-        amrex::Print() << " Flow direction : "<< ProbParm::FlowDir << "\n";    
+        pp.query("meanFlowMag", ProbParm::meanFlowMag);
+        pp.query("T_mean", ProbParm::T_mean);
+        pp.query("P_mean", ProbParm::P_mean);
+        pp.query("xblob", ProbParm::xblob);
+        pp.query("RC", ProbParm::RC);
+        pp.query("delta_blob", ProbParm::delta_blob);
+
     }
 }
