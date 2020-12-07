996d226131b7c7fdc60bb5d812fbf04d598c5248
Lucas Esclapez <lesclapez@lbl.gov>
2020-12-07 13:01:28 -0800

    Modify the EB_FlowPastCylinder to accomodate the different EB RegTests:  - a prob.type allows to select between VortexShedding case or Wave.

--- Exec/RegTests/EB_FlowPastCylinder/pelelm_prob.cpp
+++ Exec/RegTests/EB_FlowPastCylinder/pelelm_prob.cpp
@@ -10,12 +10,33 @@ extern "C" {
     {
         amrex::ParmParse pp("prob");
 
+        std::string type;
+        pp.query("type", type);
         pp.query("meanFlowMag", PeleLM::prob_parm->meanFlowMag);
         pp.query("T_mean", PeleLM::prob_parm->T_mean);
         pp.query("P_mean", PeleLM::prob_parm->P_mean);
-        pp.query("xblob", PeleLM::prob_parm->xblob);
-        pp.query("RC", PeleLM::prob_parm->RC);
-        pp.query("delta_blob", PeleLM::prob_parm->delta_blob);
+
+        if ( type == "VortexShedding" ) {
+           PeleLM::prob_parm->probType = 0;
+        } else if ( type == "Wave" ) {
+           PeleLM::prob_parm->probType = 1;
+           pp.query("xwave", PeleLM::prob_parm->xwave);
+           pp.query("RC",    PeleLM::prob_parm->RC);
+           pp.query("delta_wave", PeleLM::prob_parm->delta_wave);
+           std::string gtype;
+           pp.query("wave_type", gtype);
+           if ( gtype == "Spec" ) {
+              PeleLM::prob_parm->wave_type = 0;
+           } else if ( gtype == "Temp" ) { 
+              PeleLM::prob_parm->wave_type = 1;
+           } else {
+              amrex::Print() << " Unknown prob.wave_type ! Should be Spec or Temp \n";
+              amrex::Abort();
+           }
+        } else { 
+            amrex::Print() << " Unknown prob.type ! Should be VortexShedding or Wave \n";
+            amrex::Abort();
+        }
 
     }
 }
