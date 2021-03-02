c234e1d11b7b4f658497ff0d0f8fae5abfb3bb04
Jon Rood <jon.rood@nrel.gov>
2021-03-02 15:54:39 -0700

    Add CTestConfig file to allow submissions to CDash.

---
+++ CTestConfig.cmake
@@ -0,0 +1,6 @@
+set(CTEST_PROJECT_NAME "PeleLM")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EDT")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=PeleLM")
+set(CTEST_DROP_SITE_CDASH TRUE)
