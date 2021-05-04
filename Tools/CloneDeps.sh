5c8e52018ea8d895bfe826066e88fe74117e7f7d
Lucas Esclapez <lesclapez@lbl.gov>
2021-05-04 14:04:38 -0700

    Temp. change of the IAMR remote to allow CI.

--- Tools/CloneDeps.sh
+++ Tools/CloneDeps.sh
@@ -5,7 +5,8 @@ export PELELM_HOME=${PWD}/..
 mkdir build
 git clone https://github.com/AMReX-Codes/amrex.git build/amrex
 export AMREX_HOME=${PWD}/build/amrex
-git clone -b development https://github.com/AMReX-Codes/IAMR.git build/IAMR
+#git clone -b development https://github.com/AMReX-Codes/IAMR.git build/IAMR
+git clone -b development https://github.com/AMReX-Combustion/IAMR build/IAMR
 export IAMR_HOME=${PWD}/build/IAMR
 git clone -b development https://github.com/AMReX-Combustion/PelePhysics.git build/PelePhysics
 export PELE_PHYSICS_HOME=${PWD}/build/PelePhysics
