f205f04d09bde95ffc68d479361793fc68574d24
Lucas Esclapez <lesclapez@lbl.gov>
2021-05-04 23:00:41 -0700

    Set back CI deps. towards AMReX-Codes/IAMR.

--- Tools/CloneDeps.sh
+++ Tools/CloneDeps.sh
@@ -5,8 +5,7 @@ export PELELM_HOME=${PWD}/..
 mkdir build
 git clone https://github.com/AMReX-Codes/amrex.git build/amrex
 export AMREX_HOME=${PWD}/build/amrex
-#git clone -b development https://github.com/AMReX-Codes/IAMR.git build/IAMR
-git clone -b development https://github.com/AMReX-Combustion/IAMR build/IAMR
+git clone -b development https://github.com/AMReX-Codes/IAMR.git build/IAMR
 export IAMR_HOME=${PWD}/build/IAMR
 git clone -b development https://github.com/AMReX-Combustion/PelePhysics.git build/PelePhysics
 export PELE_PHYSICS_HOME=${PWD}/build/PelePhysics
